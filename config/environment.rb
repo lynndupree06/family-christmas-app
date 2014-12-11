0a5ee1dae9dceac7c978620ba69488d708469b0b
Jessica <jbrazelt@thoughtworks.com>
2014-12-11 16:07:12 +1100

    trying to find asset problem

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+SECRET_KEY_BASE = '2c16754ed97adb6c743785e9fba2095e6e3988177ac9768d1331ea784805e84618a717994219370576e7b61de6a78c34da7f21705be6195e5eefc1e65a2288c4'

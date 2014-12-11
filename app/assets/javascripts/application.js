ba8560d7167364dac8e1f8165cd89dd6142c5e9c
Jessica <jbrazelt@thoughtworks.com>
2014-12-11 14:46:09 +1100

    made possible fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require angular.min
 //= require angular-resource.min
 //= require angular-route.min

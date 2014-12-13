9f5a9660299e52cb20b8ba6a6d7dc1a633274afb
Jessica <jbrazelt@thoughtworks.com>
2014-12-13 14:53:03 +1100

    added glyphicon assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( lavish-bootstrap.css )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.eot )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.svg )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.ttf )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.woff )

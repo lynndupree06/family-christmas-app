256d9faeadcfe67d3614e45b9c7776dfe95adf01
Jessica <jbrazelt@thoughtworks.com>
2014-12-13 16:28:23 +1100

    fixed assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
+Rails.application.configure do
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+  config.assets.version = '1.0'
+  config.assets.precompile += %w( lavish-bootstrap.css )
+  config.assets.precompile += %w( glyphicons-halflings-regular.eot )
+  config.assets.precompile += %w( glyphicons-halflings-regular.svg )
+  config.assets.precompile += %w( glyphicons-halflings-regular.ttf )
+  config.assets.precompile += %w( glyphicons-halflings-regular.woff )
 
-# Precompile additional assets.
-# application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( *.svg *.eot *.woff *.ttf *.png *.jpg *.jpeg *.gif )
 
-Rails.application.config.assets.precompile += %w( lavish-bootstrap.css )
-Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.eot )
-Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.svg )
-Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.ttf )
-Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.woff )
+end
+
